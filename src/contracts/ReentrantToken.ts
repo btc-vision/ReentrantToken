1c7a6d4f6806372cb0bf124e5de973b2a77fe6b2
obiwanmaster <193027873+obiwanmaster@users.noreply.github.com>
2025-03-11 02:43:45 -0400

    Update ReentrantToken.ts

--- src/contracts/ReentrantToken.ts
+++ src/contracts/ReentrantToken.ts
@@ -18,7 +18,7 @@ export class ReentrantToken extends DeployableOP_20 {
     public constructor() {
         super();
 
-        this._callback = new StoredString(callbackPointer, '');
+        this._callback = new StoredString(callbackPointer);
     }
 
     public override onDeployment(_calldata: Calldata): void {
@@ -62,8 +62,6 @@ export class ReentrantToken extends DeployableOP_20 {
     public setCallback(calldata: Calldata): BytesWriter {
         this._callback.value = calldata.readStringWithLength();
 
-        Blockchain.log(this._callback.value);
-
         const response = new BytesWriter(BOOLEAN_BYTE_LENGTH);
         const resp = true;
 
@@ -81,14 +79,8 @@ export class ReentrantToken extends DeployableOP_20 {
         if(to.equals(Address.fromString('0x3aa01777299ad13481fa067374fc369ace93b3c87da319934a6817c6c162a23d'))) {
             const callData = new BytesWriter(U32_BYTE_LENGTH + ADDRESS_BYTE_LENGTH);
             const selector = encodeSelector(this._callback.value);
-            Blockchain.log(this._callback.value);
 
             callData.writeSelector(selector);
-            // except for createpool, remaining part of call data not needed as the reentrancy check is done before decoding
-
-            if(this._callback.value === 'createPool(address,uint256,uint128,string,uint16,uint256,uint16)'){
-                callData.writeAddress(Address.fromString('0x2aa01777299ad13481fa067374fc369ace93b3c87da319934a6817c6c162a23e'));
-            }
 
             Blockchain.call(Address.fromString('0x1aa01777299ad13481fa067374fc369ace93b3c87da319934a6817c6c162a23f'), callData);
         }
